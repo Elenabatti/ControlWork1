0d6715cf88308de3c8902f7b52093f426cfe0693
ELENA <battilena@gmail.com>
2022-10-03 21:23:51 +0300

    update 1

--- task/Program.cs
+++ task/Program.cs
@@ -6,4 +6,19 @@
   string[] array1 = new string[8] {"555", "seventeen", "33","one", "morning", "$", "@", "8" };
  string[] array2 = new string[array1.Length];
 
- 
+ void Array(string[] array1, string[]array2)
+{
+    int count =0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <=3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+
+
+
